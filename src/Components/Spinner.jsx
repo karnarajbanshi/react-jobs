401724369294068f88f0fd20bcbdc40409afdc4f
Shristi Koju <kojushristi4@gmail.com>
2024-10-23 17:07:25 +0545

    complete project

---
+++ src/Components/Spinner.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types'; // Import PropTypes correctly
+import ClipLoader from "react-spinners/ClipLoader";
+
+const override = {
+  display: "block",
+  margin: "100px auto",
+};
+
+const Spinner = ({ loading }) => {
+  return (
+    <ClipLoader
+      color='#4338ca'
+      loading={loading}
+      cssOverride={override}
+      size={150}
+    />
+  );
+}
+
+Spinner.propTypes = {
+  loading: PropTypes.bool.isRequired,
+};
+
+export default Spinner;
---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,11 @@
+import JobListings from '../Components/JobListings'
+
+
+const JobsPage = () => {
+  return <section className='px-4 py-6 bg-blue-50'>
+    <JobListings/>
+  </section>
+  
+}
+
+export default JobsPage
